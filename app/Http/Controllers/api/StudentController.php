b73c48b69505af5e25ccdd3b8eaffb626daaa620
Camilo Cangaro <camilocangaro@gmail.com>
2024-07-01 14:36:12 -0300

    Creating controller with methods for the API

---
+++ app/Http/Controllers/api/StudentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+
+    public function show(string $id)
+    {
+    }
+
+
+    public function update(Request $request, string $id)
+    {
+    }
+
+
+    public function destroy(string $id)
+    {
+    }
+}
